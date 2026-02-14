733da7345dc9593cd428f25903e547e12272d328
ibrahim-sheikh47 <143737834+ibrahim-sheikh47@users.noreply.github.com>
2026-02-14 23:50:24 +0500

    fix reset-pass page export

---
+++ src/app/auth/verify/page.js
@@ -0,0 +1,11 @@
+// auth/verify/page.jsx
+import React from "react";
+
+export default function VerifyPage() {
+  return (
+    <div>
+      <h1>Verify your account</h1>
+      {/* Your verify page content */}
+    </div>
+  );
+}
