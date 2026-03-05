53a3ff83a1b623c05218929c2d6294f0bce0745c
ibrahim-sheikh47 <143737834+ibrahim-sheikh47@users.noreply.github.com>
2026-03-06 03:15:45 +0500

    latest

--- src/app/splash/page.js
+++ src/app/splash/page.js
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function SplashPage() {
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace('/auth/login');
+      router.replace("/auth/login");
     }, 2500);
 
     return () => clearTimeout(timer);
@@ -24,13 +24,13 @@ export default function SplashPage() {
         height={200}
         priority
       />
-       <h1 className="text-3xl mt-5 font-semibold text-[#c7a481] mb-5">
-              My Wealth Translator
-            </h1>
-            {/* Subtitle */}
-            <h2 className="text-lg font-medium text-[#c7a481] mb-5">
-              The Financial Co-Pilot
-            </h2>
+      <h1 className="text-3xl mt-5 font-semibold text-[#c7a481] mb-5">
+        My Wealth Translator
+      </h1>
+      {/* Subtitle */}
+      <h2 className="text-lg font-medium text-[#c7a481] mb-5">
+        The Financial Co-Pilot
+      </h2>
     </div>
   );
 }
